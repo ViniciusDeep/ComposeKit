86895c5973f9f34978471dcfd5a50c8e02ee31c2
ViniciusDeep <vinicius.correia@popcode.com.br>
2020-01-08 23:02:19 -0300

    Add the idea for made views

--- ComposeKit/Compose/Composable.swift
+++ ComposeKit/Compose/Composable.swift
@@ -8,4 +8,19 @@
 
 import UIKit
 
+protocol Composable {
+    func Text(_ text: String)
+}
 
+    func Text(_ text: String) {
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10)
+        
+        ])
+        
+    }
+    
+    
+}
--- ComposeKit/ViewController.swift
+++ ComposeKit/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, Composable {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        Text("Hello world")
     }
 
 
